650fdb718f13bab49c1f9b907ec1e52f5c00d21c
Ludwig Pauly <ludwigpauly@gmail.com>
2022-05-09 23:44:24 +0200

    begin ui design 1

--- src/Memory.java
+++ src/Memory.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
+import java.io.IOException;
+
 public class Memory {
 
-    public static void main(String[]args){
+    public static void main(String[]args) throws IOException {
+        MemoryGUI memoryGUI = new MemoryGUI();
+        JFrame frame = new JFrame();
+        frame.setContentPane(memoryGUI.mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        memoryGUI.add(new JButton("a"));
+
 
     }
 
